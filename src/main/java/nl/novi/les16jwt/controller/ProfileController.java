c086d3debe5a473d79fd52ad9b8105cf99d68e0a
PimSc <pimschuitema@gmail.com>
2024-01-11 18:04:52 +0100

    commit

--- src/main/java/nl/novi/les16jwt/controller/ProfileController.java
+++ src/main/java/nl/novi/les16jwt/controller/ProfileController.java
@@ -4,7 +4,11 @@ import nl.novi.les16jwt.dto.ProfileDto;
 import nl.novi.les16jwt.model.Profile;
 import nl.novi.les16jwt.repository.ProfileRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/profiles")
@@ -33,15 +37,19 @@ public class ProfileController {
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<ProfileDto> getProfile(@PathVariable String username) {
-        Profile profile = this.repos.findById(username).get();  // happy flow
-        ProfileDto profileDto = new ProfileDto();
-        profileDto.username = profile.getUsername();
-        profileDto.firstname = profile.getFirstname();
-        profileDto.lastname  = profile.getLastname();
-        profileDto.address = profile.getAddress();
-        profileDto.bankaccount = profile.getBankAccount();
-
-        return ResponseEntity.ok(profileDto);
+    public ResponseEntity<Object> getProfile(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String username) {
+        if (Objects.equals(userDetails.getUsername(), username)) {
+            Profile profile = this.repos.findById(username).get(); // happy flow
+            ProfileDto profileDto = new ProfileDto();
+            profileDto.username = profile.getUsername();
+            profileDto.firstname = profile.getFirstname();
+            profileDto.lastname = profile.getLastname();
+            profileDto.address = profile.getAddress();
+            profileDto.bankaccount = profile.getBankAccount();
+            return ResponseEntity.ok(profileDto);
+        } else {
+// Moet exception worden!
+            return ResponseEntity.ok("INCORRECTE LOGIN");
+        }
     }
 }
